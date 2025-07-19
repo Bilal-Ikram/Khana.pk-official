8de576ae2926d1f699025db9dbff2acd6d317416
Bilal Ikram <126901173+Bilal-Ikram@users.noreply.github.com>
2025-07-19 17:14:13 +0500

    Update MenuItemDetail.jsx

--- Frontend/src/components/MenuItemDetail/MenuItemDetail.jsx
+++ Frontend/src/components/MenuItemDetail/MenuItemDetail.jsx
@@ -19,7 +19,7 @@ const MenuItemDetail = () => {
   useEffect(() => {
     const fetchMenuItem = async () => {
       try {
-        const response = await fetch(`http://localhost:3001/api/menu-items/${id}`);
+        const response = await fetch(`https://khana-backend-88zs.onrender.com/api/restaurants/api/menu-items/${id}`);
         if (!response.ok) {
           throw new Error('Failed to fetch menu item');
         }
